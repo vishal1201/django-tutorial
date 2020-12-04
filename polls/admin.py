1608b12d324242d8de961f0290e0748a48a49529
vishal <vishalzambare5@gmail.com>
2020-12-04 13:07:00 +0530

    Django Tutorial - tutorial07 1. Improved Admin filter and view page 2. Added support for templating admin view.

--- polls/admin.py
+++ polls/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
-    ]
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
 
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInLine]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
